0bb49b81b962e0bf5faa56b8229fa2d1eee437eb
tosin <tosin.sonuyi@gmail.com>
2019-04-03 13:05:48 -0400

    [WIP] parametrizing visualization functions

--- analysis.py
+++ analysis.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import pandas as pd
 
 from constants.schemas import FEC_DATA_SCHEMA
+from visualize import create_bar_chart
 
 df_kamala = pd.read_csv('data/kamala/schedule_a-2019-01-29T21_37_01.csv', dtype=FEC_DATA_SCHEMA)
 
@@ -13,10 +14,6 @@ df_warren = pd.read_csv('data/elizabeth_warren/schedule_a-2019-01-30T21_21_49.cs
 df_warren_groupedby = df_warren.groupby('contributor_name')['contribution_receipt_amount'].sum()
 
 
-import pdb
-pdb.set_trace()
-
-
 def sum_group_contributions(input_df):
     """pass in a dataframe and group it to a new series that is sorted in the order of contibutors to the campaign.
 
@@ -25,7 +22,11 @@ def sum_group_contributions(input_df):
     Arguments:
             input_df {[pandas DataFrame]} -- raw dataframe directly from the FEC site.
 
-    Returns:
+    Returns: {[pandas Series]} -- summed up contributions for each group and sorted by value
 
     """
-    return input_df.groupby('contributor_name')['contribution_receipt_amount']
+    return input_df.groupby('contributor_name')['contribution_receipt_amount'].sum().sort_values()
+
+create_bar_chart(df_warren_groupedby, top_x=10, candidate="Elizabeth Warren")
+
+create_histogram(df_warren_groupedby, top_x=10, candidate="Elizabeth Warren")
--- visualize.py
+++ visualize.py
@@ -3,5 +3,55 @@ import plotly.graph_objs as go
 from plotly import tools
 
 
-def create_bar_chart_top10(df):
+def create_bar_chart(df, top_x=None, candidate=None):
+    data = [go.Bar(
+        x=df.index,
+        y=df.values)]
+
+    title_str = 'Contributions for {}'.format(candidate)
+
+    layout = go.Layout(title=title_str,
+                       yaxis=dict(title='$ Contributions'),
+                       xaxis=dict(title='Contributor'))
+
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='{}.html'.format(title_str.replace(' ', '_')))
+
     return
+
+
+def create_histogram(df, candidate=None):
+    data = [go.Bar(
+        x=df.index,
+        y=df.values)]
+
+    title_str = 'Contributions for {}'.format(candidate)
+
+    layout = go.Layout(title=title_str,
+                       yaxis=dict(title='$ Contributions'),
+                       xaxis=dict(title='Contributor'))
+
+    fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename='{}.html'.format(title_str.replace(' ', '_')))
+
+    return
+
+
+# def non_comparative_bar(dataframe,xlabel,ylabel,title,width=0.05):
+# 	"""
+# 	Method for creating a bar plot with no comparative features.
+# 	"""
+# 	return
+
+# 	data = [go.Bar(
+# 	            x=dataframe[xlabel],
+# 	            y=dataframe[ylabel]
+# 	    )]
+
+# 	layout = go.Layout(title=title,
+# 					   yaxis=dict(
+# 					   	title=ylabel)
+# 					   )
+
+# 	fig = go.Figure(data=data,layout = layout)
+# 	py.plot(fig,filename='{}.html'.format(title))
